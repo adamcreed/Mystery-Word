d4bd8301d12cbc865dfd5a5a63ffeb89bb828562
Adam Reed <kusa.xisa@gmail.com>
2017-01-14 16:26:30 -0500

    Added win/loss and game status messages, cleaned up code.

--- mystery_word.rb
+++ mystery_word.rb
@@ -1,3 +1,61 @@
+def main
+  play_again = 'y'
+  while play_again == 'y'
+    play_again = play_game
+  end
+end
+
+def initialize_game
+  game = {}
+  game[:difficulty] = select_difficulty(game)
+  game[:mystery_word] = get_mystery_word(game).downcase
+  game[:mistakes_allowed] = 8
+  game[:letters_guessed] = []
+  game[:current_guess] = ''
+  game[:censored_word] = ''
+
+  game
+end
+
+def play_game
+  game = initialize_game
+
+  print "You can guess wrong #{game[:mistakes_allowed]} times. " \
+        "The word has #{game[:mystery_word].length} letters. " \
+        "Enter a letter: "
+
+  until game_is_over?(game)
+    game[:current_guess] = gets.chomp.downcase
+
+    unless guess_is_invalid?(game)
+      add_guess(game)
+
+      remove_chance(game) if word_does_not_contain_letter?(game)
+
+      display_partial_word(game)
+
+      display_game_status(game) unless game_is_over?(game)
+    end
+  end
+
+  reveal_word(game) if out_of_guesses?(game)
+  high_five if word_is_revealed?(game)
+  ask_if_playing_again
+end
+
+def game_is_over?(game)
+  word_is_revealed?(game) or out_of_guesses?(game)
+end
+
+def word_is_revealed?(game)
+  unspaced_censored_word = game[:censored_word].delete ' '
+  unspaced_censored_word == game[:mystery_word]
+end
+
+def out_of_guesses?(game)
+  game[:mistakes_allowed] <= 0
+end
+
 def select_difficulty(game)
   print 'Do yo want to play (E)asy, (N)ormal, or (H)ard? '
 
@@ -26,6 +84,8 @@ def get_words(game)
   full_word_list = File.readlines("/usr/share/dict/words")
                    .map { |word| word.chomp }
 
+  # I considered skipping this assignment and putting the function directly
+  # in the select below, but it looked a bit uglier.
   accepted_length = get_accepted_length(game)
 
   full_word_list.select { |word| accepted_length.include? word.length }
@@ -44,19 +104,6 @@ def get_accepted_length(game)
   end
 end
 
-def game_is_over?(game)
-  word_is_revealed?(game) or out_of_guesses?(game)
-end
-
-def word_is_revealed?(game)
-  unspaced_censored_word = game[:censored_word].delete ' '
-  unspaced_censored_word == game[:mystery_word]
-end
-
-def out_of_guesses?(game)
-  game[:mistakes_allowed] <= 0
-end
-
 def add_guess(game)
   game[:letters_guessed] << game[:current_guess]
 end
@@ -66,47 +113,58 @@ def guess_is_invalid?(game)
 end
 
 def is_not_single_letter?(current_guess)
-  if is_numeric?(current_guess) or not(current_guess.length == 1)
+  if not(current_guess =~ /[a-z]/ and current_guess.length == 1)
     print 'Enter a single letter: '
     true
-  end
-end
 
-def is_numeric?(input)
-  input.to_i.to_s == input || input.to_f.to_s == input
+  end
 end
 
 def already_guessed?(game)
   if game[:letters_guessed].include? game[:current_guess]
     print "You tried that one already, enter another letter: "
     true
-  end
-end
 
-def remove_guess(game)
-  game[:mistakes_allowed] -= 1
+  end
 end
 
 def word_does_not_contain_letter?(game)
-  unless game[:mystery_word].include? game[:letters_guessed].last
-    puts "Sorry, no #{game[:letters_guessed].last} "
+  unless game[:mystery_word].include? game[:current_guess]
+    puts "Sorry, no #{game[:current_guess]}. "
     true
-  else
-    false
+
   end
+end
+
+def remove_chance(game)
+  game[:mistakes_allowed] -= 1
 end
 
 def display_partial_word(game)
   game[:censored_word] = ''
 
   game[:mystery_word].chars.each do |char|
-
     game[:censored_word].concat(" #{char} ")
     game[:censored_word].gsub!(char, "_") unless game[:letters_guessed].include? char
 
   end
 
   puts game[:censored_word]
+end
+
+def display_game_status(game)
+  print "You have #{game[:mistakes_allowed]} " \
+        "#{plural_check(game[:mistakes_allowed], 'chance')} left. " \
+        "Previous guesses: #{game[:letters_guessed].sort.join(',')}. " \
+        "Choose another letter: "
+end
+
+def reveal_word(game)
+  print "Sorry, you're out of guesses. The word was #{game[:mystery_word]}. "
+end
+
+def high_five
+  print 'Nice job! '
 end
 
 def ask_if_playing_again
@@ -123,47 +181,26 @@ def ask_if_playing_again
   play_again
 end
 
-def display_game_status(game)
-  print "You have #{game[:mistakes_allowed]} chances left. " \
-        "Previous guesses: #{game[:letters_guessed].join(',')}. " \
-        "Choose another letter: "
-end
+def plural_check(quantity, noun)
+  if quantity.to_i == 1
+    noun
+  else
 
-def play_game
-  game = {}
-  game[:difficulty] = select_difficulty(game)
-  game[:mystery_word] = get_mystery_word(game)
-  game[:mistakes_allowed] = 8
-  game[:letters_guessed] = []
-  game[:current_guess] = ''
-  game[:censored_word] = ''
-
-  print "You can guess wrong #{game[:mistakes_allowed]} times. " \
-        "The word has #{game[:mystery_word].length} letters. " \
-        "Enter a letter: "
-
-  until game_is_over?(game)
-    game[:current_guess] = gets.chomp
-
-    unless guess_is_invalid?(game)
-      add_guess(game)
-
-      remove_guess(game) if word_does_not_contain_letter?(game)
-
-      display_partial_word(game)
-
-      display_game_status(game) unless game_is_over?(game)
+    case noun[-2..-1]
+    when /ch/, /sh/, /.s/, /.x/, /.z/
+      noun + 'es'
+    when /[aeiou]y/
+      noun + 's'
+    when /.y/
+      noun[0..-2] + 'ies'
+    when /.f/
+      noun[0..-2] + 'ves'
+    when /fe/
+      noun[0..-3] + 'ves'
+    else
+      noun + 's'
     end
   end
-
-  ask_if_playing_again
 end
 
-def main
-  play_again = 'y'
-  while play_again == 'y'
-    play_again = play_game
-  end
-end
-
-main
+main if __FILE__ == $PROGRAM_NAME
