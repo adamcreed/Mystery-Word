4b83dab591cf9dc8c98544ed5d0f3bb9543f70ef
Adam Reed <kusa.xisa@gmail.com>
2017-01-14 12:21:40 -0500

    Implemented letter entry, changed variables to hash.

--- mystery_word.rb
+++ mystery_word.rb
@@ -1,36 +1,38 @@
-def select_difficulty
+def select_difficulty(game)
   print 'Do yo want to play (E)asy, (N)ormal, or (H)ard? '
 
-  game_difficulty = ''
+  game[:difficulty] = ''
 
-  until game_difficulty =~ /[enh]/
-    game_difficulty = get_first_char
+  until game[:difficulty] =~ /[enh]/
+    game[:difficulty] = get_first_char
 
-    puts "Please choose easy, normal, or hard" unless game_difficulty =~ /[enh]/
+    puts "Please choose easy, normal, or hard" \
+          unless game[:difficulty] =~ /[enh]/
+
   end
 
-  game_difficulty
+  game[:difficulty]
 end
 
 def get_first_char
   gets.chomp[0].downcase
 end
 
-def get_mystery_word(game_difficulty)
-  get_words(game_difficulty).sample
+def get_mystery_word(game)
+  get_words(game).sample
 end
 
-def get_words(game_difficulty)
+def get_words(game)
   full_word_list = File.readlines("/usr/share/dict/words")
                    .map { |word| word.chomp }
 
-  accepted_length = get_accepted_length(game_difficulty)
+  accepted_length = get_accepted_length(game)
 
   full_word_list.select { |word| accepted_length.include? word.length }
 end
 
-def get_accepted_length(game_difficulty)
-  case game_difficulty
+def get_accepted_length(game)
+  case game[:difficulty]
   when 'e'
     4..6
 
@@ -42,21 +44,57 @@ def get_accepted_length(game_difficulty)
   end
 end
 
+def game_is_over?(game)
+  word_is_revealed?(game) or out_of_guesses?(game)
+end
+
+def word_is_revealed?(game)
+  unspaced_censored_word = game[:censored_word].delete ' '
+  unspaced_censored_word == game[:mystery_word]
+end
+
+def out_of_guesses?(game)
+  game[:mistakes_allowed] <= 0
+end
+
+def add_guess(game)
+  game[:letters_guessed] << get_guess(game)
+end
+
+def get_guess(game)
+  until is_single_letter?(current_guess = gets.chomp)
+    print 'Enter a single letter: '
+  end
+  current_guess
+end
+
+def is_single_letter?(current_guess)
+  not(is_numeric?(current_guess)) and current_guess.length == 1
+end
+
+def is_numeric?(input)
+  input.to_i.to_s == input || input.to_f.to_s == input
+end
+
 def play_game
-  game_difficulty = select_difficulty
-  mystery_word = get_mystery_word(game_difficulty)
-  guesses_allowed = 8
-  letters_guessed = []
+  game = {}
+  game[:difficulty] = select_difficulty(game)
+  game[:mystery_word] = get_mystery_word(game)
+  game[:mistakes_allowed] = 8
+  game[:letters_guessed] = []
 
-  print " You can guess wrong #{guesses_allowed} times. "\
-          "The word has #{mystery_word.length} letters. Enter a letter: "
+  print "You can guess wrong #{game[:mistakes_allowed]} times. " \
+        "The word has #{game[:mystery_word].length} letters. " \
+        "Enter a letter: "
 
-#   until game_is_over?
-#     add_guess(letters_guessed)
+# until game_is_over?
+  add_guess(game)
+  p game[:letters_guessed]
+
 #
-#     remove_guess if word_does_not_contain_letter?(letters_guessed.last)
+#     remove_guess if word_does_not_contain_letter?(game)
 #
-#     display_partial_word(letters_guessed)
+#     display_partial_word(game)
 #   end
 #
 #   play_again?
